18f04e77794cf856ce984c8cb86d6312e608adc1
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2023-01-13 10:59:28 -0600

    adding auth

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import SettingsProvider from "./Context/SettingsContext";
 import { MantineProvider } from "@mantine/core";
+import AuthProvider from "./Context/Auth";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
+      <AuthProvider>
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
+      </AuthProvider>
     </MantineProvider>
   </React.StrictMode>
 );
